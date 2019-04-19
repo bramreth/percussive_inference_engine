944fd826f2b07581fc615bd8a6fd2f981ce6213f
bram <bramreth@gmail.com>
2019-04-19 23:19:46 +0100

    added primitive wav reading and some plans for beat detection
    note i should investigate magenta tensorflow for drum generation once i have detected the audio bpm

--- input_handler.py
+++ input_handler.py
@@ -1,10 +1,58 @@
-
+import wave, struct, array, os
+#http://doc.sagemath.org/html/en/reference/misc/sage/media/wav.html
 class InputHandler:
+    params = []
+
+
     def __init__(self, args):
         print(args.target)
         print(args.test)
-
+        self.validate_target(args.target)
 
     # take the input
-    def validate_target(self):
-        pass
+    def validate_target(self, target):
+        waveFile = wave.open(target, 'r')
+
+        length = waveFile.getnframes()
+
+        # print details
+        self.params = waveFile.getparams()
+        print(self.params)
+
+        # track length = nframes / framerate
+        track_length = self.params[3] / self.params[2]
+        print(track_length)
+
+        #this calculates the size of a sample, to parse the binary data to two signed itnegers
+        sizes = {1: 'B', 2: 'h', 4: 'i'}
+        channels = waveFile.getnchannels()
+        fmt_size = sizes[waveFile.getsampwidth()]
+        fmt = "<" + fmt_size * channels
+
+        ret = []
+        # save the values read from the wav as tuples in a list, this is very expensive and needs improvement. see:
+        # https://stackoverflow.com/questions/5804052/improve-speed-of-reading-and-converting-from-binary-file
+        # https://www.cameronmacleod.com/blog/reading-wave-python
+        while waveFile.tell() < waveFile.getnframes():
+            decoded = struct.unpack(fmt, waveFile.readframes(1))
+            #print(decoded)
+            ret.append(decoded)
+
+
+        """
+        
+        Bram's assumptions about the data from wav files.
+        so they are the status of a wave at a certain time across the left and right stereo channels.
+        the wave must have a minimum and a maximum. they appear to be at a fixed framerate, so i should
+        be able to multiply the current frame by the rate and obtain the time that value was at. 
+        
+        the numbers are very similar due to me not having much stereo going on.
+        
+        it seems safe to assume larger nmbers are peaks? i should be able to find turning points and mark them
+        as potential beats. then interpolate these beats over time.
+        
+        here is some beat detection with numpy
+        https://stackoverflow.com/questions/12344951/detect-beat-and-play-wav-file-in-a-synchronised-manner
+        
+        """
+        waveFile.close()
