7ded682a27c45572c344c22f555f06b280e9a4d8
Bram <bramreth@gmail.com>
2019-04-25 20:55:08 +0100

    completed tempo inference and primitive midi drum track creation
    testing is best performed with a suitable daw

--- librosa_analysis.py
+++ librosa_analysis.py
@@ -1,5 +1,9 @@
 import librosa
 import librosa.display
+from music21 import stream, instrument
+from music21.note import Note
+from midi2audio import FluidSynth
+import os, time
 
 def analyse_file(filename):
     # y is the time series as a numpy array
@@ -19,6 +23,43 @@ def analyse_file(filename):
     #display graphs for analysis
     librosa.display.waveplot(y)
 
+
+
+    drumPart = stream.Part()
+
+    bass = instrument.BassDrum()
+    snare = instrument.SnareDrum()
+    drumPart.insert(0, bass)
+    drumPart.insert(1, snare)
+
+    i = 0
+
+    drumMeasure = stream.Measure()
+    for item in beat_times:
+
+        n = Note("A2")
+        i = not i
+
+        if i:
+            #the standard note for a kick drum
+            n = Note("C2")
+            
+        else:
+
+            #standard for a snare
+            n = Note("D2")
+        n.offset = item
+        drumMeasure.append(n)
+    path = 'beat_file/beat_file.mid'
+    drumPart.append(drumMeasure)
+    fp = drumPart.write('midi', fp=path)
+    # This line actually generate the midi on my mac but there is no relevant software to read it and the opening fail
+    #drumPart.show('midi')
+    #while not os.path.exists(path):
+    #    time.sleep(1)
+    #fs = FluidSynth()
+    #FluidSynth().play_midi(path)
+    #fs.midi_to_audio(path, "tst.wav")
 
 #http://conference.scipy.org/proceedings/scipy2015/pdfs/brian_mcfee.pdf
 #https://librosa.github.io/librosa/tutorial.html
